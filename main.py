3b2ad737c2f121768ab4ba4b365933e7ba2cdb4c
reynardsaputra <ge35qum@mytum.de>
2024-07-24 17:40:00 +0200

    added main file

---
+++ main.py
@@ -0,0 +1,19 @@
+from utils import read_video, save_video
+from trackers import Tracker
+
+
+def main():
+    # Read Video
+    video_frames = read_video("input_videos/test_video.mp4")
+
+    # Initialize Tracker
+    tracker = Tracker("models/bundesliga/best.pt")
+
+    tracks = tracker.get_object_tracks(video_frames)
+
+    # Save Video
+    save_video(video_frames, "output_videos/output_video.avi")
+
+
+if __name__ == "__main__":
+    main()
