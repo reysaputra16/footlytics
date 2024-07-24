289ef6a713a2f4c86b049a4a2f243048c81aa0bc
reynardsaputra <ge35qum@mytum.de>
2024-07-24 17:36:44 +0200

    added best and last model after training from notebook

--- yolo_inference.py
+++ yolo_inference.py
@@ -1,6 +1,10 @@
 from ultralytics import YOLO
 
-model = YOLO("yolov8m")
+original_model = "yolov8m.pt"
+bundesliga_model_best = "models/bundesliga/best.pt"
+bundesliga_model_last = "models/bundesliga/last.pt"
+
+model = YOLO(bundesliga_model_best)
 
 results = model.predict("input_videos/test_video.mp4", save=True)
 print(results[0])
