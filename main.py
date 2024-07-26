a7926e330b2b194738ce5cbaf5bf482e25f00d68
reynardsaputra <ge35qum@mytum.de>
2024-07-26 21:11:45 +0200

    added code with drawing annotations on bbox

--- main.py
+++ main.py
@@ -10,11 +10,15 @@ def main():
     tracker = Tracker("models/bundesliga/best.pt")
 
     tracks = tracker.get_object_tracks(
-        video_frames, read_from_stub=True, stub_path="stubs/track_stubs.pkl"
+        video_frames, read_from_stub=True, stub_path="stubs/tracks_stubl.pkl"
     )
 
+    # Draw Output
+    ## Draw object tracks
+    output_video_frames = tracker.draw_annotations(video_frames, tracks)
+
     # Save Video
-    save_video(video_frames, "output_videos/output_video.avi")
+    save_video(output_video_frames, "output_videos/output_video.avi")
 
 
 if __name__ == "__main__":
